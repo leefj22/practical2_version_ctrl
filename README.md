eac2a9f320975416337b274ac45cb23318ec7847
leefj22 <leefj-wm22@student.tarc.edu.my>
2025-07-03 12:48:07 +0800

    Updated main screen UI with name

---
+++ README.md
@@ -0,0 +1,3 @@
+# practical2_version_ctrl
+
+A new Flutter project with version control.
---
+++ android/app/src/main/kotlin/my/edu/tarc/practical2_version_ctrl/MainActivity.kt
@@ -0,0 +1,5 @@
+package my.edu.tarc.practical2_version_ctrl
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
